9db70a8d72852ebcc316a59a5f4d1752b18f0509
Aaryash-Shakya <100826663+Aaryash-Shakya@users.noreply.github.com>
2024-04-08 16:00:10 +0545

    init: prisma database, scheme and migration

---
+++ prisma/migrations/20240408101430_init/migration.sql
@@ -0,0 +1,22 @@
+-- CreateTable
+CREATE TABLE "Category" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "categoryId" INTEGER NOT NULL,
+    "name" TEXT NOT NULL,
+    "price" REAL NOT NULL,
+    "description" TEXT,
+    "image" TEXT,
+    "imageCount" INTEGER NOT NULL DEFAULT 0,
+    "location" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    CONSTRAINT "Product_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
